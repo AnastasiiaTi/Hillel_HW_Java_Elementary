9d619133eb5bb762d49e16a4b26c007e16f56ad6
AnastasiiaTi <1901.nastya@gmail.com>
2021-10-13 16:17:24 +0300

    homework3 added

---
+++ src/Lessons/homework3/Person.java
@@ -0,0 +1,32 @@
+package Lessons.homework3;
+
+public class Person {
+
+    String surname;
+    String name;
+    String patronymic;
+
+    public Person(String name, String surname, String patronymic) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+    }
+
+    public Person(String surnameNamePatronymic) {
+        String[] fullNameOfThePerson;
+        String delimeter = " ";
+        fullNameOfThePerson = surnameNamePatronymic.split(delimeter);
+        this.surname = fullNameOfThePerson[0];
+        this.name = fullNameOfThePerson[1];
+        this.patronymic = fullNameOfThePerson[2];
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                '}';
+    }
+}
